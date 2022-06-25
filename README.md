62d881031dbd9a888fb6a4e2eab79497b94d3987
kundan9576 <karan.kundan061@gmail.com>
2022-06-25 15:10:39 +0530

    project completed

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 Project Given by: Ziroh Labs
+
 There is a hypothetical application ‘Secure Vault’, where users can store their files securely.
 You are given the task to develop the ‘Secure Photos’ component of the application by referring
 to the UX (Next page).
+
 Functionalities to be implemented -
+
 ● As the ‘Secure Photos’ component gets loaded, fetch the photos and display them using
 the following API -
 `https://jsonplaceholder.typicode.com/albums/1/photos`
+
 ● Also, allow the user to delete any item from the page.
 
 Project Setup
@@ -17,10 +21,10 @@ Step 3: In one run command: ng serve --open
 
 Step 4: Open your browser and type: http://localhost:4200
 
-Project Documentation:
-https://docs.google.com/document/d/1NdB3SdAbKSFcPooap-ddpHdQQxBDvAieH2bPF4NCLMU/edit?usp=sharing
+Project Live: (Hosted on AWS)
 
+https://prod.dmz1hvrqz3omn.amplifyapp.com/#/home
 
-Author: Kundan Kumar Singh
-Mobile Number: 8789967038
+Author: Kundan Kumar Singh,
+Mobile Number: 8789967038,
 email: karan.kundan061@gmail.com
--- src/app/components/secure-photos/secure-photos.component.ts
+++ src/app/components/secure-photos/secure-photos.component.ts
@@ -12,18 +12,59 @@ export class SecurePhotosComponent implements OnInit {
   constructor( private _ApiRequestService: ApiRequestService ) { }
 
   photosList:any = [];
+  searchPhotos: string = '';
+  searchPhotoList:any = [];
 
   ngOnInit(): void {
     this._ApiRequestService.requestPhotoApi().subscribe(res => {
       this.photosList = res;
-    });
+      this.searchPhotoList = res;
+    }); // we are trigerring the api to get photos list
 
-    this._ApiRequestService.navOpen.next(1);
+    this._ApiRequestService.navOpen.next(1); // for changing the value of the navbar icon
   }
 
-  deleteImage( index ){
-    this.photosList.splice( index, 1 );
+  deleteImage(index, photoTitle) {
+    // when there is no value in searched field we are deleting from main list and storedList;
+    if (!this.searchPhotos) {
+      this.photosList.splice(index, 1); 
+      this.searchPhotoList.splice( index, 1); //
+    } 
+    else {
+      let deleteIndex_1; // to delete in main stored list
+      for (let i = 0; i < this.searchPhotoList.length; i++) {
+        if (this.searchPhotoList[i].title === photoTitle ) {
+          deleteIndex_1 = i;
+        }
+      }
+      // when we found the same title in the main list
+      if( deleteIndex_1 >= 0 ){
+        this.searchPhotoList.splice( deleteIndex_1, 1 );
+      }
+      // splicing the found title
+
+      this.photosList.splice( index, 1 );
+      // deleting the index from  searched title
+    }
   }
 
+
+  seachValueChange(value) {
+
+    if (!value) {
+      this.photosList = this.searchPhotoList; // when there is nothing in search field all value should come
+    }
+    else {
+      let searchedList = [];
+      for (let i = 0; i < this.searchPhotoList.length; i++) {
+        if (this.searchPhotoList[i].title.includes(value)) {
+          searchedList.push(this.searchPhotoList[i]);
+        }
+      }
+      this.photosList = searchedList;
+      // when we found some title in searched list
+    }
+
+  }
 
 }
